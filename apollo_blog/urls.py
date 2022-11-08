8afd8fe707ec31ba7bb7d197c54e9d763efb3e2b
BXS-Archipelago <71372294+BXS-Archipelago@users.noreply.github.com>
2022-11-08 13:22:04 +0000

    update base and about pages

--- apollo_blog/urls.py
+++ apollo_blog/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('about/', about, name='about'),
+    path('', index, name='index'),
 ]
