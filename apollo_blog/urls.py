622b0883417bbad3140dc54d8bb7f4fe78aeb835
BXS-Archipelago <71372294+BXS-Archipelago@users.noreply.github.com>
2022-11-09 11:24:37 +0000

    create blog/detail.html, add blog urls and views

--- apollo_blog/urls.py
+++ apollo_blog/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
-from .views import index, about
+from django.urls import path, include
+from apollo_blog.views import index, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about, name='about'),
+    path('', include('blog.urls'), name='about'),
     path('', index, name='index'),
 ]
